14c3d20548690498d8d29af9907cb79ca5c1620d
Abhay-kashyap <abhaykashyap265@gmail.com>
2026-05-30 13:31:56 +0530

    02-components

---
+++ 02_component/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+      <div className="card">
+        <h1>Abhay Kashyap</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae accusantium iure optio hic adipisci dolore repellendus totam voluptatum sequi minima. Similique a dolores quos dolor. Reprehenderit deleniti quam quis! Repellat. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio reiciendis similique magnam, eveniet excepturi deleniti hic molestiae veritatis. Minima, eaque rem earum illum tempora corporis quos veritatis saepe alias esse? </p>
+      </div>
+    </>
+  )
+}
+
+export default Card
